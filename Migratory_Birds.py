3c7bfdef98d65370412c5a116463443cce96fecd
Mihir192 <105259127+Mihir192@users.noreply.github.com>
2022-05-11 12:36:42 +0530

    Create Migratory_Birds.py

---
+++ Migratory_Birds.py
@@ -0,0 +1,70 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    lst2 = []
+    for i in range(1, 6):
+        x = arr.count(i)
+        lst2.append(x)
+    for i in range(1, 6):
+        x = arr.count(i)
+        if x == max(lst2):
+            return(i)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    lst2 = []
+    for i in range(1, 6):
+        x = arr.count(i)
+        lst2.append(x)
+    for i in range(1, 6):
+        x = arr.count(i)
+        if x == max(lst2):
+            return(i)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
