5269262ed8de658998feaed027b174006f977db6
Mihir192 <105259127+Mihir192@users.noreply.github.com>
2022-05-25 08:33:45 +0530

    Create Viral_Advertising.py

---
+++ Viral_Advertising.py
@@ -0,0 +1,34 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    x = 5
+    l = 0
+    sum1 = 0
+    for i in range(n):
+        l = x // 2
+        sum1 = sum1 + l
+        x = l * 3
+    return sum1
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
